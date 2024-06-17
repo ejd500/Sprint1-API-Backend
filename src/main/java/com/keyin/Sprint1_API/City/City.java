1ed9471b609ce6effbebcbaaecb906439c79e588
Corina Jewer <corina.jewer@keyin.com>
2024-06-17 15:15:57 -0230

    Updated The 4 Entity Classes to include constructors, getters/setters.

--- src/main/java/com/keyin/Sprint1_API/City/City.java
+++ src/main/java/com/keyin/Sprint1_API/City/City.java
@@ -9,7 +9,55 @@ public class City {
 
     private int city_id;
     private String name;
-    private String state;
+    private String province;
     private int population;
-    private List<Airport> airports = new ArrayList<Airport>();
+    private List<Airport> airports;
+
+    public City(int city_id, String name, String province, int population) {
+        this.city_id = city_id;
+        this.name = name;
+        this.province = province;
+        this.population = population;
+        this.airports = new ArrayList<Airport>();
+    }
+
+    public int getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(int city_id) {
+        this.city_id = city_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public List<Airport> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<Airport> airports) {
+        this.airports = airports;
+    }
 }
--- src/main/java/com/keyin/Sprint1_API/Passenger/Passenger.java
+++ src/main/java/com/keyin/Sprint1_API/Passenger/Passenger.java
@@ -13,6 +13,62 @@ public class Passenger {
     private String lastName;
     private String phoneNum;
     private City city;
-    private List<Aircraft> aircrafts = new ArrayList<Aircraft>();
+    private List<Aircraft> aircraft;
 
+    public Passenger(int passenger_id, String firstName, String lastName, String phoneNum, City city) {
+        this.passenger_id = passenger_id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.city = city;
+        this.aircraft = new ArrayList<Aircraft>();
+    }
+
+    public int getPassenger_id() {
+        return passenger_id;
+    }
+
+    public void setPassenger_id(int passenger_id) {
+        this.passenger_id = passenger_id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public List<Aircraft> getAircraft() {
+        return aircraft;
+    }
+
+    public void setAircraft(List<Aircraft> aircraft) {
+        this.aircraft = aircraft;
+    }
 }
