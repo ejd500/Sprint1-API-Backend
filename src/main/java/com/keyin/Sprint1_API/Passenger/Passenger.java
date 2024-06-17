9e041c06f0ea9f13d065830bacd269630120b980
Corina Jewer <corina.jewer@keyin.com>
2024-06-17 13:38:03 -0230

    Updated 4 Entity Classes

--- src/main/java/com/keyin/Sprint1_API/Passenger/Passenger.java
+++ src/main/java/com/keyin/Sprint1_API/Passenger/Passenger.java
@@ -1,4 +1,18 @@
 package com.keyin.Sprint1_API.Passenger;
 
+import com.keyin.Sprint1_API.Aircraft.Aircraft;
+import com.keyin.Sprint1_API.City.City;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Passenger {
+
+    private int passenger_id;
+    private String firstName;
+    private String lastName;
+    private String phoneNum;
+    private City city;
+    private List<Aircraft> aircrafts = new ArrayList<Aircraft>();
+
 }
