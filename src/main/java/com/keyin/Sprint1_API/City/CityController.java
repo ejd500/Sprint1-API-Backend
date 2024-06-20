33c54d99a540e4040438ab7f305f1b3cccef4b1a
ejd500 <ellen.dalton-dawe@keyin.com>
2024-06-20 07:09:38 -0230

    Added CityController and CityService. Amended City.

---
+++ src/main/java/com/keyin/Sprint1_API/City/CityController.java
@@ -0,0 +1,37 @@
+package com.keyin.Sprint1_API.City;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@CrossOrigin
+public class CityController {
+    @Autowired
+    private CityService cityService;
+
+    @PostMapping("create_city")
+    public City createCity(@RequestBody City newCity) {
+        return cityService.createCity(newCity);
+    }
+
+    @GetMapping("cities")
+    public List<City> getAllCities() {
+        return cityService.getAllCities();
+    }
+
+    @GetMapping("city/{index}")
+    public City getCity(@PathVariable Integer index) {
+        return CityService.getCity(index);
+    }
+
+    @PutMapping("city/{index}")
+    public City updateCity(@PathVariable Integer index, @RequestBody City updatedCity) {
+        return cityService.updateCity(index, updatedCity);
+    }
+
+    @DeleteMapping("city/{index}")
+    public void deleteCity(@PathVariable Integer index) {
+        cityService.deleteCity(index);
+    }
+}
