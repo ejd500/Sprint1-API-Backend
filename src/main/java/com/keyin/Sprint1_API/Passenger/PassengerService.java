68a75953dfa1313ed3a6a641d48ca98bf0ae9306
ejd500 <ellen.dalton-dawe@keyin.com>
2024-06-24 15:25:21 -0230

    Implemented Create Passenger

--- src/main/java/com/keyin/Sprint1_API/Passenger/PassengerService.java
+++ src/main/java/com/keyin/Sprint1_API/Passenger/PassengerService.java
@@ -1,6 +1,8 @@
 package com.keyin.Sprint1_API.Passenger;
 
 import com.keyin.Sprint1_API.City.City;
+import com.keyin.Sprint1_API.City.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,26 +10,30 @@ import java.util.List;
 
 @Service
 public class PassengerService {
+
+    private int nextId = 1;
+
     private List<Passenger> passengers = new ArrayList<>();
 
+    @Autowired
+    private CityService cityService;
+
     public Passenger createPassenger(Passenger newPassenger){
-        if(passengers.isEmpty()){
-            passengers.add(newPassenger);
-            return newPassenger;
-        } else {
-            for(Passenger passenger: passengers){
-                if(passenger.getFirstName().equalsIgnoreCase(newPassenger.getFirstName()) &&
-                        passenger.getLastName().equalsIgnoreCase(newPassenger.getLastName()) &&
-                        passenger.getPhoneNum().equalsIgnoreCase(newPassenger.getPhoneNum()) &&
-                        passenger.getCity().equals(newPassenger.getCity())
-                ){
-                    newPassenger = null;
-                    return newPassenger;
-                }
+        for(Passenger passenger: passengers) {
+            if (passenger.getFirstName().equalsIgnoreCase(newPassenger.getFirstName()) &&
+                    passenger.getLastName().equalsIgnoreCase(newPassenger.getLastName()) &&
+                    passenger.getPhoneNum().equalsIgnoreCase(newPassenger.getPhoneNum()) &&
+                    passenger.getCity().getName().equalsIgnoreCase(newPassenger.getCity().getName()) &&
+                    passenger.getCity().getProvince().equalsIgnoreCase(newPassenger.getCity().getProvince()) &&
+                    passenger.getCity().getPopulation() == newPassenger.getCity().getPopulation()
+            ) {
+                return passenger;
             }
-            passengers.add(newPassenger);
-            return newPassenger;
         }
+        City city = cityService.createCity(newPassenger.getCity());
+        Passenger passengerToCreate = new Passenger(nextId++, newPassenger.getFirstName(), newPassenger.getLastName(), newPassenger.getPhoneNum(), city);
+        passengers.add(passengerToCreate);
+        return passengerToCreate;
     }
 
     public List<Passenger> getAllPassengers(){
